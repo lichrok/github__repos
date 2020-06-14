71ad51e10ba8295da779554c427ed90e4e8b1c25
lichrok <s.dmitry.dev@gmail.com>
2020-06-14 19:34:46 +0300

    removed console.log

--- src/components/TopicsList/index.js
+++ src/components/TopicsList/index.js
@@ -4,7 +4,6 @@ import './styles.scss';
 
 export const TopicsList = ({ topics }: TopicsListProps) => (
 	<ul className="topics-list">
-		{console.log(topics)}
 		{topics.map(({ node }) => <li key={node.topic.id}>{node.topic.name}</li>)}
 	</ul>
 );
